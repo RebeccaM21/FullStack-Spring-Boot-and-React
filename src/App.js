25524206310249b7b1f80851dbb14e8a3ba65092
Rebecca Mpi <rebecca.mpi@imgarena.com>
2023-09-04 15:26:57 +0100

    Completed functionality in application

--- src/App.js
+++ src/App.js
@@ -1,13 +1,24 @@
-import './App.css';
+import "./App.css";
 import "../node_modules/bootstrap/dist/css/bootstrap.min.css";
-import Navbar from './layout/Navbar';
-import Home from './pages/Home';
+import AddUser from "./users/AddUser";
+import Navbar from "./layout/Navbar";
+import Home from "./pages/Home";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import EditUser from "./users/EditUser";
+import ViewUser from "./users/ViewUser";
 
 function App() {
   return (
     <div className="App">
-      <Navbar/>
-      <Home/>
+      <Router>
+        <Navbar />
+        <Routes>
+          <Route exact path="/" element={<Home />}></Route>
+          <Route exact path="/addUser" element={<AddUser />}></Route>
+          <Route exact path="/editUser/:id" element={<EditUser />}></Route>
+          <Route exact path="/viewUser/:id" element={<ViewUser />}></Route>
+        </Routes>
+      </Router>
     </div>
   );
 }
